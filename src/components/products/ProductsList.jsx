86a3b032c29c4d10d1f9788eb6852c5d3852125a
asimov <seyranmustafa4@gmail.com>
2024-09-02 20:03:06 +0300

    p

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, Row } from "react-bootstrap";
 import ProductCard from "./ProductCard";
 import "./Products.scss";
-import { products,} from "../../helper/data";
+import { products } from "../../helper/data";
 import { Header } from "../header/Header";
 
 const ProductsList = ({}) => {
@@ -16,13 +16,9 @@ const ProductsList = ({}) => {
       />
       <Container className="product-list rounded-4 my-4 p-3">
         <Row className="g-3 justify-content-center">
-{
-  products.map((item)=>(
-    <ProductCard key={item.id} item={item}/>
-  ))
-}
-
-          
+          {products.map((item) => (
+            <ProductCard key={item.id} item={item} />
+          ))}
         </Row>
       </Container>
     </>
