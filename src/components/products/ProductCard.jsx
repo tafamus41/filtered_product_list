4023ded51a799237155968ecaff2e30c8e23803c
asimov <seyranmustafa4@gmail.com>
2024-09-03 03:39:19 +0300

    update

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ image, id, description, title, price }) => {
     <Col sm={12} md={6} lg={4} xl={3}>
       <Card key={id} className="rounded-2 m-auto card" role="button">
         <Card.Header className="d-flex justify-content-between">
-          <Card.Title>{price}$</Card.Title>
+          <Card.Title>{price} $</Card.Title>
           <MdFavorite size={30} />
         </Card.Header>
         <Card.Img variant="top" src={image} className="player-logo" />
