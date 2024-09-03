c2b9ce5500911d70ff0e1172ebd0783b98f42405
asimov <seyranmustafa4@gmail.com>
2024-09-03 04:13:06 +0300

    p

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Container, Stack } from "react-bootstrap";
 import "./Header.scss";
 import { categories } from "../../helper/data";
+// import { useState } from "react";
 
 export const Header = () => {
+// const[category,setCategory]=useState("");
+// const[buton,setButon]=useState("");
+// const handleCategory=()=>{
+
+// }
   // console.log(categories);
   return (
     <Container className="header">
@@ -14,7 +20,7 @@ export const Header = () => {
         className="btns justify-content-center flex-md-row"
       >
         {categories.map((item) => (
-          <button key={item} className="">{item.toUpperCase()}</button>
+          <button onClick={(e)=>{console.log(e.target.textContent)}} key={item} className="">{item.toUpperCase()}</button>
         ))}
       </Stack>
     </Container>
